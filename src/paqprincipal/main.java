f4486cc7fe845efc0265602b0a9a8149fc8fa424
Jordy QC <94817291+Jordy-21@users.noreply.github.com>
2023-07-23 21:56:55 -0500

    Herencias de Cliente y Trabajador

--- src/paqprincipal/main.java
+++ src/paqprincipal/main.java
@@ -3,14 +3,28 @@ package paqprincipal;
 
 public class main {
     public static void main(String[] args) {
-        Persona persona = new Persona();
-        persona.setEdad(26);
-        persona.setNombre("Carlitos");
-        persona.setTelefono(2345567);
+        Cliente cliente = new Cliente();
+        cliente.setEdad(10);
+        cliente.setNombre("Carlitos");
+        cliente.setTelefono(98652136);
+        cliente.setCredito(20);
 
-        System.out.println(persona.getEdad());
-        System.out.println(persona.getNombre());
-        System.out.println(persona.getTelefono());
+        System.out.println(cliente.getEdad());
+        System.out.println(cliente.getNombre());
+        System.out.println(cliente.getTelefono());
+        System.out.println(cliente.getCredito());
+
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.setEdad(2);
+        trabajador.setNombre("Juan");
+        trabajador.setTelefono(98632136);
+        trabajador.setSalario(5500);
+
+        System.out.println(trabajador.getEdad());
+        System.out.println(trabajador.getNombre());
+        System.out.println(trabajador.getTelefono());
+        System.out.println(trabajador.getSalario());
     }
 }
 class Persona{
@@ -41,4 +55,30 @@ class Persona{
     public int getTelefono(){
         return this.telefono;
     }
+
 }
+
+class Cliente extends Persona{
+    private int credito;
+
+    public int getCredito() {
+        return credito;
+    }
+
+    public void setCredito(int credito) {
+        this.credito = credito;
+    }
+}
+
+class Trabajador extends Persona{
+    private float salario;
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+}
+
